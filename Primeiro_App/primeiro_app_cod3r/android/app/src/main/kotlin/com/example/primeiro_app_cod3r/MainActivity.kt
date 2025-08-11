12d70574cc501618469cb798756806d2b7df22b1
Guilherme <116519824+gui-ccr@users.noreply.github.com>
2025-08-11 10:45:55 -0300

    Initialize Flutter app project structure
    Add initial files for a new Flutter project including platform-specific directories and configuration for Android, iOS, Linux, macOS, Windows, and web. Includes build scripts, resource files, icons, and basic Dart entry point.

---
+++ Primeiro_App/primeiro_app_cod3r/android/app/src/main/kotlin/com/example/primeiro_app_cod3r/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.primeiro_app_cod3r
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Primeiro_App/primeiro_app_cod3r/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text("Olá Mundo"));
+  }
+}
